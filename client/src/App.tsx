552862b570e66c6ace9ae977f17261e155ea3e6b
ahmed samir <ahmedsamirweb42@gmail.com>
2024-08-19 23:52:11 +0300

    add localization system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import { Button, Box, useTheme } from "@mui/material";
 import { useAppDispatch } from "./redux/hooks";
 import { toggleTheme } from "./redux/themeSlice";
+import { useTranslation } from "react-i18next";
 
 const App: React.FC = () => {
   const theme = useTheme();
   const dispatch = useAppDispatch();
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
 
   const handleToggleTheme = () => {
     dispatch(toggleTheme());
@@ -16,7 +22,13 @@ const App: React.FC = () => {
         Toggle {theme.palette.mode === "light" ? "Dark" : "Light"} Mode
       </Button>
       <Box sx={{ mt: 2 }}>Current mode: {theme.palette.mode}</Box>
-      {/* Add your other components here */}
+      <Box sx={{ mt: 2 }}>
+        <Button onClick={() => changeLanguage("en")} sx={{ mr: 1 }}>
+          English
+        </Button>
+        <Button onClick={() => changeLanguage("ar")}>ar</Button>
+      </Box>
+      {t("test")}
     </Box>
   );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,12 +5,16 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.ts";
 import ThemeWrapper from "./components/theme/ThemeWrapper.tsx";
 import { PersistGate } from "redux-persist/integration/react";
+import "./i18n.ts";
+import { Suspense } from "react";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <ThemeWrapper>
-        <App />
+        <Suspense>
+          <App />
+        </Suspense>
       </ThemeWrapper>
     </PersistGate>
   </Provider>
